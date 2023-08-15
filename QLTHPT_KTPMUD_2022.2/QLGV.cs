fc56be32174320b4d42080610923c35efc296915
luhen168 <trung2992003@gmail.com>
2023-08-15 21:11:50 +0700

    up qlgv

--- QLTHPT_KTPMUD_2022.2/QLGV.cs
+++ QLTHPT_KTPMUD_2022.2/QLGV.cs
@@ -50,7 +50,7 @@ namespace QLTHPT_KTPMUD_2022._2
         public QLGV()
         {
             InitializeComponent();
-            //comment;gghelllo
+            //comment;gghelllokkkkkkvvvvvvvvvv
         }
 
         private void QLGV_Load(object sender, EventArgs e)
